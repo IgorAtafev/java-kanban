a410f0289f2820319852e97f57ab1a4f323ef1f0
IgorAstafev <programmer77@yandex.ru>
2022-11-23 02:18:24 +0300

    feat: make class InMemoryHistoryManagerTest

---
+++ test/ru/yandex/practicum/tasktracker/manager/InMemoryHistoryManagerTest.java
@@ -0,0 +1,24 @@
+package ru.yandex.practicum.tasktracker.manager;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InMemoryHistoryManagerTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void removeAll() {
+    }
+
+    @Test
+    void getHistory() {
+    }
+}
