6fa490e057c6aaad53c0935179834d4d69419616
IgorAstafev <programmer77@yandex.ru>
2022-11-23 12:33:55 +0300

    feat: add tests

--- test/ru/yandex/practicum/tasktracker/manager/InMemoryHistoryManagerTest.java
+++ test/ru/yandex/practicum/tasktracker/manager/InMemoryHistoryManagerTest.java
@@ -1,24 +1,103 @@
 package ru.yandex.practicum.tasktracker.manager;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.tasktracker.model.Task;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.HashSet;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class InMemoryHistoryManagerTest {
+    private Task task1;
+    private Task task2;
+    private Task task3;
 
-    @Test
-    void add() {
+    private final HistoryManager historyManager = new InMemoryHistoryManager();
+
+    @BeforeEach
+    public void setUp() {
+        task1 = createTask(1);
+        task2 = createTask(2);
+        task3 = createTask(3);
     }
 
     @Test
-    void remove() {
+    public void getHistory_shouldReturnEmptyHistory() {
+        assertTrue(historyManager.getHistory().isEmpty());
     }
 
     @Test
-    void removeAll() {
+    public void add_shouldSaveTaskToHistory() {
+        historyManager.add(task1);
+        historyManager.add(task2);
+        historyManager.add(task3);
+
+        List<Task> expected = List.of(task1, task2, task3);
+        List<Task> actual = historyManager.getHistory();
+
+        assertEquals(expected, actual);
     }
 
     @Test
-    void getHistory() {
+    public void add_shouldNotKeepDuplicates() {
+        historyManager.add(task1);
+        historyManager.add(task2);
+        historyManager.add(task3);
+        historyManager.add(task3);
+
+        List<Task> expected = List.of(task1, task2, task3);
+        List<Task> actual = historyManager.getHistory();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void add_shouldMoveTaskToTheEnd_ifTaskAlreadyExistsInHistory() {
+        historyManager.add(task1);
+        historyManager.add(task2);
+        historyManager.add(task3);
+        historyManager.add(task1);
+
+        List<Task> expected = List.of(task2, task3, task1);
+        List<Task> actual = historyManager.getHistory();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void remove_shouldRemoveTaskFromHistory() {
+        historyManager.add(task1);
+        historyManager.add(task2);
+        historyManager.add(task3);
+
+        historyManager.remove(2);
+
+        List<Task> expected = List.of(task1, task3);
+        List<Task> actual = historyManager.getHistory();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void removeAll_shouldRemoveTasksFromHistory() {
+        historyManager.add(task1);
+        historyManager.add(task2);
+        historyManager.add(task3);
+
+        historyManager.removeAll(new HashSet<>(List.of(1, 3)));
+
+        List<Task> expected = List.of(task2);
+        List<Task> actual = historyManager.getHistory();
+
+        assertEquals(expected, actual);
+    }
+
+    private static Task createTask(int id) {
+        Task task = new Task();
+        task.setId(id);
+        return task;
     }
 }
