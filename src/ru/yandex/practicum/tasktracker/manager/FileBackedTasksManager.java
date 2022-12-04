e77582c337dbbeda55eb24ea59efe5bfd70b781e
IgorAstafev <programmer77@yandex.ru>
2022-12-05 02:39:45 +0300

    feat: create FileBackedTasksManager

---
+++ src/ru/yandex/practicum/tasktracker/manager/FileBackedTasksManager.java
@@ -0,0 +1,91 @@
+package ru.yandex.practicum.tasktracker.manager;
+
+import ru.yandex.practicum.tasktracker.model.Epic;
+import ru.yandex.practicum.tasktracker.model.SubTask;
+import ru.yandex.practicum.tasktracker.model.Task;
+
+import java.nio.file.Path;
+
+public class FileBackedTasksManager extends InMemoryTaskManager {
+    private final Path filename;
+
+    public FileBackedTasksManager(String filename) {
+        this.filename = Path.of(filename);
+    }
+
+    @Override
+    public void deleteTasks() {
+        super.deleteTasks();
+        save();
+    }
+
+    @Override
+    public void deleteEpics() {
+        super.deleteEpics();
+        save();
+    }
+
+    @Override
+    public void deleteSubTasks() {
+        super.deleteSubTasks();
+        save();
+    }
+
+    @Override
+    public void deleteTaskById(int id) {
+        super.deleteTaskById(id);
+        save();
+    }
+
+    @Override
+    public void deleteEpicById(int id) {
+        super.deleteEpicById(id);
+        save();
+    }
+
+    @Override
+    public void deleteSubTaskById(int id) {
+        super.deleteSubTaskById(id);
+        save();
+    }
+
+    @Override
+    public void createTask(Task task) {
+        super.createTask(task);
+        save();
+    }
+
+    @Override
+    public void createEpic(Epic epic) {
+        super.createEpic(epic);
+        save();
+    }
+
+    @Override
+    public void createSubTask(SubTask subTask) {
+        super.createSubTask(subTask);
+        save();
+    }
+
+    @Override
+    public void updateTask(Task task) {
+        super.updateTask(task);
+        save();
+    }
+
+    @Override
+    public void updateEpic(Epic epic) {
+        super.updateEpic(epic);
+        save();
+    }
+
+    @Override
+    public void updateSubTask(SubTask subTask) {
+        super.updateSubTask(subTask);
+        save();
+    }
+
+    private void save() {
+
+    }
+}
