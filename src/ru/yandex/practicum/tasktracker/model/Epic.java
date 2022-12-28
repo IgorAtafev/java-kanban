a53b3fbe963e9f624d82fb0e3b1879d9f79e9fbb
IgorAstafev <programmer77@yandex.ru>
2022-12-28 19:36:53 +0300

    fix: fixed bugs in Manager and Model

--- src/ru/yandex/practicum/tasktracker/model/Epic.java
+++ src/ru/yandex/practicum/tasktracker/model/Epic.java
@@ -7,7 +7,6 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
-import java.util.stream.Stream;
 
 public class Epic extends Task {
     private final Set<SubTask> subTasks = new LinkedHashSet<>();
