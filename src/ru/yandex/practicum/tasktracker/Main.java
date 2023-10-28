5c11a2746411b0d2a3cc9c31d5a3932afe934d17
IgorAstafev <programmer77@yandex.ru>
2023-10-28 16:43:47 +0300

    fix: Main

--- src/ru/yandex/practicum/tasktracker/Main.java
+++ src/ru/yandex/practicum/tasktracker/Main.java
@@ -13,7 +13,6 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
         KVServer server = new KVServer();
         server.start();
         TaskManager taskManager = Managers.getDefault();
